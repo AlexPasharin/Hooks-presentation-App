c97b4a793b273aa1e12e340dd75c8276859fe661
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-08-29 20:41:55 +0300

    ref and use layout effect

--- client/src/components/Entries/Entry.jsx
+++ client/src/components/Entries/Entry.jsx
@@ -1,17 +1,18 @@
-import React, { Component, createRef, useState } from 'react'
+import React, { Component, createRef, useState, useRef, useLayoutEffect } from 'react'
 import { formatDate } from '../../utils/dataHelpers'
 import { classList } from '../../utils/classList'
 
 import EntryTracks from "./EntryTracks"
 
-export default class Entry extends Component {
+//export default
+class Entry extends Component {
   state = {
     open: false,
   }
 
   el = createRef()
 
-  async componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (!prevProps.selected && this.props.selected) {
       this.el.current.focus()
     }
@@ -78,21 +79,57 @@ export default class Entry extends Component {
   }
 }
 
-const EntryFunc = ({ entry, selected }) => {
+const EntryFunc = ({ entry, selected, selectPrevEntry, selectNextEntry, select }) => {
   const [open, setOpen] = useState(false)
   const { name, release_date, id } = entry
+
+  const el = useRef()
+
+  useLayoutEffect(() => {
+    if (selected) el.current.focus()
+  }, [selected])
+
+  useLayoutEffect(() => {
+    if (open) el.current.scrollIntoView()
+  }, [open])
+
+  const toggleTracksBlock = () => {
+    setOpen(prevState => !prevState)
+  }
+
+  const onKeyDown = e => {
+    e.preventDefault()
+
+    const { key } = e
+
+    if (key === 'ArrowUp') {
+      if (!open) {
+        selectPrevEntry()
+      }
+    } else if (key === 'ArrowDown') {
+      if (!open) {
+        selectNextEntry()
+      }
+    } else if (key === "Enter") {
+      toggleTracksBlock()
+    }
+  }
+
+  const onFocus = () => {
+    if (!selected)
+      select()
+  }
 
   return (
     <li
       tabIndex="0"
-      ref={this.el}
+      ref={el}
       className={classList("entry-block", { open, selected }, ["no-focus-outline"])}
-      onKeyDown={this.onKeyDown}
-      onFocus={this.onFocus}
-
+      onKeyDown={onKeyDown}
+      onFocus={onFocus}
     >
       <div className="entry-block__details"
-        onClick={this.toggleTracksBlock}
+        onClick={toggleTracksBlock}
       >
         <h2>{name} </h2>
         <p>
@@ -105,4 +142,4 @@ const EntryFunc = ({ entry, selected }) => {
   )
 }
 
-// export default EntryFunc
+export default EntryFunc
