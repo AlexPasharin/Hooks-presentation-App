a94124631076e5f151e359ddcaf1af448fc045c6
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-08-29 20:10:21 +0300

    App as functional component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { getArtists, getTypes } from "../src/utils/dataGetters"
 import Main from "./components/Entries/Main"
 
 import './styles/App.css'
 
-export default class App extends React.Component {
+//export default
+class App extends React.Component {
   state = {
     artists: null,
     types: null
@@ -36,3 +37,40 @@ export default class App extends React.Component {
     );
   }
 }
+
+// const asyncFunc = async () => {
+//   const [artists, types] = await Promise.all([
+//     getArtists(),
+//     getTypes()
+//   ])
+
+//   setArtists(artists)
+//   setTypes(types)
+// }
+
+const AppFunc = () => {
+  const [artists, setArtists] = useState(null)
+  const [types, setTypes] = useState(null)
+
+  useEffect(() => {
+    Promise.all([
+      getArtists(),
+      getTypes()
+    ]).then(([artists, types]) => {
+      setArtists(artists)
+      setTypes(types)
+    })
+  }, [])
+
+  if (!artists || !types)
+    return "Loading data..."
+
+  return (
+    <Main
+      artists={artists}
+      types={types}
+    />
+  );
+}
+
+export default AppFunc
