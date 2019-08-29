2aaa61dd05fc4fbad905460a6329ae9478d8de7d
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-08-29 20:18:02 +0300

    main to functional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,39 +5,6 @@ import Main from "./components/Entries/Main"
 
 import './styles/App.css'
 
-//export default
-class App extends React.Component {
-  state = {
-    artists: null,
-    types: null
-  }
-
-  async componentDidMount() {
-    const [artists, types] = await Promise.all([
-      getArtists(),
-      getTypes()
-    ])
-
-    this.setState({
-      artists,
-      types,
-    })
-  }
-
-  render() {
-    const { artists, types } = this.state
-    if (!artists || !types)
-      return "Loading data..."
-
-    return (
-      <Main
-        artists={artists}
-        types={types}
-      />
-    );
-  }
-}
-
 // const asyncFunc = async () => {
 //   const [artists, types] = await Promise.all([
 //     getArtists(),
--- client/src/components/Entries/Main.jsx
+++ client/src/components/Entries/Main.jsx
@@ -1,79 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { getEntries } from "../../utils/dataGetters"
 import NavBar from '../NavBar/NavBar'
 import Entries from './Entries'
 
-//export default
-class EntriesMain extends React.Component {
-  state = {
-    selectedArtist: null,
-    selectedType: null,
-    entries: [],
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { selectedArtist: prevSelectedArtist, selectedType: prevSelectedType } = prevState
-    const { selectedArtist, selectedType } = this.state
-
-    if (
-      selectedArtist !== prevSelectedArtist ||
-      selectedType !== prevSelectedType
-    ) {
-      this.updateEntries()
-    }
-  }
-
-  onSelectArtist = selectedArtist => {
-    this.setState({ selectedArtist })
-  }
-
-  onSelectType = selectedType => {
-    this.setState({ selectedType })
-  }
-
-  updateEntries = () => {
-    const { selectedArtist, selectedType } = this.state
-
-    if (!selectedArtist || !selectedType) {
-      this.setState({ entries: [] })
-    } else {
-      this.setState({ entries: null })
-      getEntries(selectedArtist, selectedType).then(
-        entries => this.setState({ entries })
-      )
-    }
-
-  }
-
-  render() {
-    const { artists, types } = this.props
-    const { selectedArtist, selectedType, entries } = this.state
-
-    return (
-      <div className="main-content">
-        <NavBar
-          artists={artists}
-          types={types}
-          selectedArtist={selectedArtist}
-          selectedType={selectedType}
-          onSelectArtist={this.onSelectArtist}
-          onSelectType={this.onSelectType}
-        />
-        {/* <main>
-          <Entries
-            entries={entries}
-          />
-        </main> */}
-      </div>
-    )
-  }
-}
 
 const MainFunc = ({ artists, types }) => {
-
   const [selectedArtist, setSelectedArtist] = useState(null)
   const [selectedType, setSelectedType] = useState(null)
+  const [entries, setEntries] = useState([])
+
+  useEffect(() => {
+    if (!selectedArtist || !selectedType) {
+      setEntries([])
+    } else {
+      setEntries(null)
+      getEntries(selectedArtist, selectedType).then(setEntries)
+    }
+  }, [selectedArtist, selectedType])
 
 
   return (
@@ -86,11 +30,11 @@ const MainFunc = ({ artists, types }) => {
         onSelectArtist={setSelectedArtist}
         onSelectType={setSelectedType}
       />
-      {/* <main>
+      <main>
         <Entries
           entries={entries}
         />
-      </main> */}
+      </main>
     </div>
   )
 
