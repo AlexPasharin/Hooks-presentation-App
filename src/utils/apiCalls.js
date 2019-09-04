15b43874c2205c5648f110fd68cc3438de4d9e2a
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-09-04 20:34:27 +0300

    resturcture

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'http://localhost:2000'
+
+const fetchData = (resource, method, body, headers) =>
+  fetch(`${BASE_URL}/rest/${resource}`, {
+    method: method || 'GET',
+    body: body ? JSON.stringify(body) : undefined,
+    headers
+  })
+    .then(response => response.json())
+
+
+export const fetchArtists = () => fetchData('artists')
+export const fetchTypes = () => fetchData('types')
+export const fetchEntries = (artistID, typeID) => fetchData(`entries?artistID=${artistID}&typeID=${typeID}`)
+export const fetchTracks = albumID => fetchData(`tracks?albumID=${albumID}`)
