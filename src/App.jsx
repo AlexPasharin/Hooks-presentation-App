f55f4f23c77355a449453031301b8b17ce778092
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-09-05 17:54:35 +0300

    return class implementation of App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,39 @@ import Main from "./components/Entries/Main"
 
 import './styles/App.css'
 
+
+class App extends React.Component {
+  state = {
+    artists: null,
+    types: null
+  }
+
+  async componentDidMount() {
+    const [artists, types] = await Promise.all([
+      getArtists(),
+      getTypes()
+    ])
+
+    this.setState({
+      artists,
+      types,
+    })
+  }
+
+  render() {
+    const { artists, types } = this.state
+    if (!artists || !types)
+      return "Loading data..."
+
+    return (
+      <Main
+        artists={artists}
+        types={types}
+      />
+    );
+  }
+}
+
 // here App is re-implemented in a functional style
 const AppFunc = () => {
   const [artists, setArtists] = useState(null)
